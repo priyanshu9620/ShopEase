7606720db5f339d33c14bbc4f1990e18853071ff
PRIYANSHU GUPTA <78604252+priyanshu9620@users.noreply.github.com>
2024-09-23 19:13:40 +0530

    first commit

---
+++ helpers/authHelper.js
@@ -0,0 +1,16 @@
+import bcrypt from "bcrypt";
+
+export const hashPassword = async (password) => {
+  try {
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    return hashedPassword;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// as done in prev code here also you can create a variable and use await no problem
+export const comparePassword = async (password, hashedPassword) => {
+  return bcrypt.compare(password, hashedPassword);
+};
---
+++ routes/authRoute.js
@@ -0,0 +1,62 @@
+import express from "express";
+import {
+  registerController,
+  loginController,
+  testController,
+  forgotPasswordController,
+  updateProfileController,
+  getOrdersController,
+  getAllOrdersController,
+  orderStatusController,
+  getuserController,
+} from "../controllers/authController.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+
+//router object
+const router = express.Router();
+
+//routing
+//REGISTER || METHOD POST
+router.post("/register", registerController);
+
+//LOGIN || POST
+router.post("/login", loginController);
+
+//Forgot Password || POST
+router.post("/forgot-password", forgotPasswordController);
+
+//update profile
+router.put("/profile", requireSignIn, updateProfileController);
+
+
+
+//all users
+router.get("/get-user", getuserController);
+
+//orders
+router.get("/orders", requireSignIn, getOrdersController);
+
+//all orders
+router.get("/all-orders", requireSignIn, isAdmin, getAllOrdersController);
+
+// order status update
+router.put("/order-status/:orderId",requireSignIn,isAdmin,orderStatusController);
+
+
+
+// leave these three routes just cheking (of isadmin and signed in user )
+//test routes
+router.get("/test", requireSignIn, isAdmin, testController);
+
+//protected User route auth
+router.get("/user-auth", requireSignIn, (req, res) => {
+  res.status(200).send({ ok: true });
+});
+//protected Admin route auth
+router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
+  res.status(200).send({ ok: true });
+});
+
+
+
+export default router;
